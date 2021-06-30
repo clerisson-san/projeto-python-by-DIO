a192022dde11cea27bc7ec92db6ce41902f99dd3
clerisson.san <clerisson@mt.estudante.senai.br>
2021-06-29 23:24:04 -0400

    Quinto Commit: Alteração Arquivo my_app.py

---
+++ app_python/meu_app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+#!/usr/bin/env python
+# coding: utf-8
+#!/usr/bin/env python
+# coding: utf-8
+print("Programa Python")
+a = 2
+b = 9
+soma = a + b
+print(soma)
